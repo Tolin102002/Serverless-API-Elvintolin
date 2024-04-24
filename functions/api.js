ab1174444812c10cfbff0064198677ab8980208b
Elvin <elvintolin@gmail.com>
2024-04-24 12:57:51 +0800

    Initial commit

---
+++ functions/api.js
@@ -0,0 +1,21 @@
+const express = require('express');
+const serverless = require('serverless-http');
+const router = require('./routes/author');
+const mongoose = require('mongoose');
+const cors = require('cors');
+const app = express();
+
+const cloudDB = 'mongodb+srv://tolinelvin:tolinelvin10@tolin10.9kuhrnt.mongodb.net/';
+const localDB = 'mongodb://localhost:27017/serverless-api/tolin-api';
+
+app.use(cors());
+app.use(express.json());
+app.use(express.urlencoded({extended:true}));
+
+mongoose
+  .connect(cloudDB || localDB)
+  .then(()=> console.log('Connected to MongoDB'))
+  .catch((error)=>console.error('Failed to connect to MongoDB'));
+
+app.use('/.netlify/functions/api', router);
+module.exports.handler = serverless(app);
