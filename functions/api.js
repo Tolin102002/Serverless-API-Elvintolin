a7ea0f2ddc43d33f684993a386a3700064107314
Elvin <elvintolin@gmail.com>
2024-04-24 13:54:37 +0800

    Commit message

--- functions/api.js
+++ functions/api.js
@@ -6,8 +6,8 @@ const cors = require('cors');
 
 const app = express();
 // your mongodb Cloud URL
-const dbCloudUrl = "mongodb+srv://fetizananjohnkenneth:Johnkenn23@cluster0.dmpyias.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0";
-const dbLocalUrl = 'mongodb://localhost:27017/express-mongo-api';
+const dbCloudUrl = "mongodb+srv://tolinelvin:tolinelvin10@tolin10.9kuhrnt.mongodb.net/?retryWrites=true&w=majority&appName=Tolin10";
+const dbLocalUrl = 'mongodb://localhost:27017/serverless-api';
 
 app.use(cors());
 app.use(express.json());
